e44c987b69d8289c8bab26d71115d21fb739f102
jpablocasanueva <jpablocasanueva@gmail.com>
2022-02-22 16:43:07 -0300

    Clase 12, ultimad de nivelacion JS

---
+++ 02-intro-javascript/src/bases/12-operdorTernario.js
@@ -0,0 +1,21 @@
+// Operacion condicional ternario
+
+const active = true;
+
+// FormaTradicional
+
+// let mensaje = '';
+
+// if( !activo ) {
+//     mensaje = 'activo';
+// } else {
+//     mensaje = 'inactivo';
+// } 
+
+
+// Con Operador Ternario
+
+const message = ( active ) ? 'Active' : 'Inactive';
+
+console.log(message);
+
